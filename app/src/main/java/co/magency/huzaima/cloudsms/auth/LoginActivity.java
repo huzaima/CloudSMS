60086e42a11a1b765aa2b2d0ddd1d0dadddb400a
Huzaima Khan <khanhuzaima@gmail.com>
2017-03-07 21:32:51 +0500

    intermediate

--- app/src/main/java/co/magency/huzaima/cloudsms/auth/LoginActivity.java
+++ app/src/main/java/co/magency/huzaima/cloudsms/auth/LoginActivity.java
@@ -2,24 +2,28 @@ package co.magency.huzaima.cloudsms.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 
 import co.magency.huzaima.cloudsms.R;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView email, password;
     private Button login;
+    private Socket socket;
     //    private IntentIntegrator qrScanner;
     private final String LOG_TAG = LoginActivity.class.getSimpleName();
 
@@ -41,21 +45,70 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        mAuth.signInWithEmailAndPassword(email.getText().toString().trim(),
-                password.getText().toString().trim())
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful())
-                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-                        else {
-                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), QrCodeScannerActivity.class));
-//                            qrScanner.initiateScan();
+//        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+//        mAuth.signInWithEmailAndPassword(email.getText().toString().trim(),
+//                password.getText().toString().trim())
+//                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (!task.isSuccessful())
+//                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+//                        else {
+//                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+//                            startActivity(new Intent(getApplicationContext(), QrCodeScannerActivity.class));
+////                            qrScanner.initiateScan();
+//                        }
+//                    }
+//                });
+
+        try {
+            socket = IO.socket("http://192.168.1.2:3000");
+            socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(LoginActivity.this, "Connected", Toast.LENGTH_SHORT).show();
                         }
+                    });
+                    try {
+                        socket.emit("abc", (new JSONObject()).put("abc", "abc").put("id", socket.id()));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                });
+                }
+            });
+
+            socket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(LoginActivity.this, "Disconnect", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+
+            socket.on("abc", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(LoginActivity.this, "abc called", Toast.LENGTH_SHORT).show();
+                            Log.v("lalala", "abc called");
+                        }
+                    });
+                }
+            });
+
+            socket.connect();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
